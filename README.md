51f8c8f5c26e03b8564357ac115e30185546ab42
kamrulhaider <kamrulhaider.ssb@gmail.com>
2021-09-15 02:24:59 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# charity_new
+
+https://towhidhasan01.github.io/charity_new/
+# ru-charity
---
+++ js/header.js
@@ -0,0 +1,21 @@
+let a = 0;
+function showDiv(id, id2, id3) {
+    const value = a++;
+    if (value === 0) {
+        document.getElementById(id).style.display = 'block';
+    } else {
+        document.getElementById(id).style.display = 'none';
+        document.getElementById(id2).style.display = 'none';
+        document.getElementById(id3).style.display = 'none';
+        a = 0;
+    }
+};
+function showDiv2(id, id2) {
+    document.getElementById(id).style.display = 'block';
+    document.getElementById(id2).style.display = 'none';
+};
+
+function closeAll() {
+    document.getElementById('dropdown-content').style.display = 'none';
+    a = 0;
+};
